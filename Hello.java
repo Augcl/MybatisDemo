39153d66a609bf7dfec9cca9f630c6609948d5c4
Aug <119095762+Augcl@users.noreply.github.com>
2024-06-05 19:05:44 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    /**
+     * 文档注释
+     * @param args
+     */
+    public static void main(String[] args){
+        //单行注释
+        /*
+        多行注释
+        多行注释
+        多行注释
+         */
+        System.out.println("hello");
+    }
+}
